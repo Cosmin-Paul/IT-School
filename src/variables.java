86760f6fbbe0d9128924faef2606c3419b512d33
Cosmin <pcosmin_17@yahoo.com>
2024-06-10 19:25:46 +0300

    tema 1

--- src/variables.java
+++ src/variables.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class variables {
+public class variables {
+    public static void main(String[] args) {
+        int age = 55;
+        double temperature = 25.5d;
+        char grade = 'A';
+        String name = "Hello World!";
+
+        System.out.println("age:" + age);
+        System.out.println("temperature:" + temperature);
+        System.out.println("name:" + name);
+
+
+
+
+    }
 }
