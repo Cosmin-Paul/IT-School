c9caedc57a4a1018027a500783f8d255654109db
Cosmin <pcosmin_17@yahoo.com>
2024-06-13 18:40:52 +0300

    session 3 - java operators

---
+++ src/session_3_java_operators/challenges/ch3_1.java
@@ -0,0 +1,21 @@
+package session_3_java_operators.challenges;
+
+public class ch3_1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int difference = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+        int modulus = a % b;
+
+        System.out.println("the sum of numbers:" + sum);
+        System.out.println("the difference of numbers:" + difference);
+        System.out.println("the multiplication of numbers:" + multiplication);
+        System.out.println("the division of numbers:" + division);
+        System.out.println("the modulus of numbers:" + modulus
+        );
+
+    }
+}
