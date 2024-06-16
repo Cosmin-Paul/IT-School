df3aa0ec380af20e73be67abf8933f1b2a7cb483
Cosmin <pcosmin_17@yahoo.com>
2024-06-16 20:38:12 +0300

    session 3 - challenges of the 3rd course  update

---
+++ src/session_3_java_operators/challenges/ch3_12.java
@@ -0,0 +1,19 @@
+package session_3_java_operators.challenges;
+
+import java.util.Scanner;
+
+public class ch3_12 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer!:");
+        int numberInteger = scanner.nextInt();
+
+        numberInteger++;
+        System.out.println("The value of number incremented are:" + numberInteger);
+
+        numberInteger--;
+        System.out.println("The value of number decremented are:" + numberInteger);
+    }
+}
