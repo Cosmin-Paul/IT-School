250ba501fee9432aae8a931eb4ea537d0517d85a
Cosmin <pcosmin_17@yahoo.com>
2024-06-17 18:30:19 +0300

    challenges of the 4th course  update

---
+++ src/session_4_java_operators_and_loops/challenges/ch4_12.java
@@ -0,0 +1,13 @@
+package session_4_java_operators_and_loops.challenges;
+
+public class ch4_12 {
+    public static void main(String[] args) {
+
+        int x = 25;
+
+        int negativeNumber = -x;
+
+        System.out.println("The original number is: " + x);
+        System.out.println("The negative number is: " + negativeNumber);
+    }
+}
