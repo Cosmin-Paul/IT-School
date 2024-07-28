94b158735da29b5b510e23b60e086fb7dfe0f0fc
Cosmin <pcosmin_17@yahoo.com>
2024-07-28 14:44:01 +0300

    updates

---
+++ src/session_16_java_collections_framework/challenges/Challenge_15.java
@@ -0,0 +1,31 @@
+package session_16_java_collections_framework.challenges;
+
+import java.util.ArrayList;
+
+public class Challenge_15 {
+    public static void main(String[] args) {
+        ArrayList<String> travelDestination1 = new ArrayList<>();
+        travelDestination1.add("Palma de Mallorca");
+        travelDestination1.add("Barcelona");
+        travelDestination1.add("Ciudad Del Cruz");
+        travelDestination1.add("Thailand");
+        travelDestination1.add("Verona");
+
+        System.out.println("The travel destination1 are: " + travelDestination1);
+
+        ArrayList<String> travelDestination2 = new ArrayList<>();
+        travelDestination2.add("Sydney Australia");
+        travelDestination2.add("Yukatan Mexico");
+        travelDestination2.add("Madagascar");
+        travelDestination2.add("Paris");
+        travelDestination2.add("Cancun");
+
+        System.out.println("The travel destination2 are:" + travelDestination2);
+
+        ArrayList<String> joinedList = new ArrayList<>();
+        joinedList.addAll(travelDestination1);
+        joinedList.addAll(travelDestination2);
+
+        System.out.println("The joined list of destinations are: " + joinedList);
+    }
+}
---
+++ src/session_16_java_collections_framework/challenges/Challenge_8.java
@@ -0,0 +1,22 @@
+package session_16_java_collections_framework.challenges;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Challenge_8 {
+    public static void main(String[] args) {
+        ArrayList<String>fruitList = new ArrayList<>();
+        fruitList.add("Kaki");
+        fruitList.add("Grapes");
+        fruitList.add("Passion fruit");
+        fruitList.add("Mango");
+        fruitList.add("Cherry");
+        fruitList.add("Pomegrante");
+        fruitList.add("Ananas");
+
+        Collections.sort(fruitList);
+
+        System.out.println(fruitList);
+    }
+
+}
