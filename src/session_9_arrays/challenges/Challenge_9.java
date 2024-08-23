167d67ae9145e033d371bea1cb7e90c753c58a51
Cosmin <pcosmin_17@yahoo.com>
2024-08-23 20:01:55 +0300

    commit

---
+++ src/session_9_arrays/challenges/Challenge_9.java
@@ -0,0 +1,10 @@
+package session_9_arrays.challenges;
+
+public class Challenge_9 {
+    public static void main(String[] args) {
+        String binaryNumber = "1001";
+        int number = Integer.parseInt(binaryNumber, 4);
+
+        System.out.println("The decimal representation of the " + binaryNumber+ " is:    " +number);
+    }
+}
